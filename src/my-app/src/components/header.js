6d254814420307af1fa9b4af68e8a66f16ee948b
YanissB <yaniss.besson@student.adatechschool.fr>
2022-01-10 17:01:58 +0100

    Test react + Journal d'Équipe

---
+++ src/my-app/src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <ul>
+            <li>Ada Tech</li>
+            <li>React</li>
+            <li>Fun</li>
+            <li>School</li>
+        </ul>
+    );
+};
+
+export default Header;
