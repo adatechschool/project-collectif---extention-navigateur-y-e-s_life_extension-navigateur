bcce6b03987c6cbadb19438fd6ae8c5517d5d8c5
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2022-01-11 16:30:53 +0100

    change color background > to color h1

--- je-debute/popup.js
+++ je-debute/popup.js
@@ -19,6 +19,6 @@ changeColor.addEventListener("click", async () => {
   // current page
   function setPageBackgroundColor() {
     chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
+      document.querySelector("h1").style.color = color;
     });
   };
