3ffeb58b350fad47d329b9b237708dafbd405a5e
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2022-01-11 15:56:28 +0100

    Change background color

---
+++ je-debute/background.js
@@ -0,0 +1,8 @@
+let color = '#ecce6d';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+
