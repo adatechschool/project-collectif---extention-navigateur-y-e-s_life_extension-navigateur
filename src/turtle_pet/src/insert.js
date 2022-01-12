796515b611b52b2a779d86e6065ab12865079737
YanissB <yaniss.besson@student.adatechschool.fr>
2022-01-12 20:30:58 +0100

    cleaning + turtle pet wip

---
+++ src/turtle_pet/src/insert.js
@@ -0,0 +1,6 @@
+alert("cc")
+let turtleDiv = document.createElement("div");
+turtleDiv.id = "turtlePet";
+turtleDiv.style = "z-index: 99999; position: fixed; top: 802px; right: 42px;";
+document.body.appendChild(turtleDiv);
+turtleDiv.innerHTML = "<img src= "/src/imgTurtle/idleTurtle.jpg">";
