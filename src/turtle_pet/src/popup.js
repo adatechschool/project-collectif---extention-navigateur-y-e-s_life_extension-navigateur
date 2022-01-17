fd04d04e6316bc1816045004e14facf2db1a9024
Soso-M <80459553+Soso-M@users.noreply.github.com>
2022-01-17 16:41:54 +0100

    feat: hungryTurtle works !!!

--- src/turtle_pet/src/popup.js
+++ src/turtle_pet/src/popup.js
@@ -1,4 +1,7 @@
 let displayTurtle = document.getElementById("display");
+let date = new Date();
+let minutes = date.getMinutes();
+let randomNumber = Math.floor(Math.random() * 6);
 displayTurtle.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
@@ -16,4 +19,41 @@ const setDisplayTurtle = () => {
   turtleImg.src = chrome.runtime.getURL("/imgTurtle/idleTurtle2.png");
   turtleDiv.appendChild(turtleImg);
   document.body.appendChild(turtleDiv);
+  function hungryTurtle() {
+    let getHungry = 4
+    if (getHungry === 4) {
+      getHungry = 0
+      return true
+    }
+    else {
+      if (minutes === 0 && randomNumber === 1) {
+        getHungry = 0
+        return true
+      }
+      else {
+        getHungry++
+      }
+    }
+  }
+console.log(hungryTurtle());
 };
+
+/*function hungryTurtle(name){
+  if(name === "sonia")
+  return 2
+  console.log(hungryTurtle("sonia"));
+  let getHungry = 4
+  if (getHungry === 4) {
+    getHungry = 0
+    return true
+  }
+  else {
+    if (minutes === 0 && randomNumber === 1) {
+      getHungry = 0
+      return true
+    }
+    else {
+      getHungry++
+    }
+  }
+}*/
