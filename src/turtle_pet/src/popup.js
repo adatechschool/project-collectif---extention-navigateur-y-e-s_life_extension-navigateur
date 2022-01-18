febb9968daaa1713e531cc81041f69aad2433603
YanissB <yaniss.besson@student.adatechschool.fr>
2022-01-18 16:58:49 +0100

    Feat startTime + All comportements works

--- src/turtle_pet/src/popup.js
+++ src/turtle_pet/src/popup.js
@@ -1,4 +1,6 @@
 let displayTurtle = document.getElementById("display");
+
+//Execute script when clicked on popup's extension button "coucou"
 displayTurtle.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
@@ -7,85 +9,131 @@ displayTurtle.addEventListener("click", async () => {
   });
 });
 
+// Display turtle on the html with div
 const setDisplayTurtle = () => {
   alert("cc");
   let turtleDiv = document.createElement("div");
   turtleDiv.id = "turtlePet";
   turtleDiv.style = "z-index: 99999; position: fixed; top: 802px; right: 42px;";
   let turtleImg = document.createElement("img");
-  turtleImg.src = chrome.runtime.getURL("/imgTurtle/idleTurtle2.png");
+  turtleImg.src = chrome.runtime.getURL("/imgTurtle/idleTurtle.png");
   turtleDiv.appendChild(turtleImg);
   document.body.appendChild(turtleDiv);
-  let date = new Date();
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
+  
+  let today
+  let h
+  let m
+  let s 
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+  function startTime() {
+    // add a zero in front of numbers<10
+    today = new Date();
+    h = today.getHours();
+    m = today.getMinutes();
+    s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    t = setTimeout(function() {
+      startTime()
+    }, 2000);
+
+    updateTurtle();
+  
+  }
+
+
+  
   let randomNumber = Math.floor(Math.random() * 6);
 
+  //All turtle's behaviors functions
 
-console.log(isTurtlePooped());
-}
-// function hungryTurtle() {
-//   let getHungry = 0
-//   if (getHungry === 4) {
-//     getHungry = 0
-//     return true
-//   }
-//   else {
-//     if (minutes === 49) {
-//       getHungry = 0
-//       return true
-//     }
-//     else {
-//       getHungry++
-//       return false
-//     }
-//   }
-// }
+  function isTurtleHungry() {
+    let getHungry = 0;
+    if (getHungry === 4) {
+      getHungry = 0;
+      return true;
+    } else {
+      if (m === 4  && s === 0) {
+        getHungry = 0;
+        return true;
+      } else {
+        if ((m = 0 && s === 0)) {
+          getHungry++;
+        }
+        console.log("cc1");
+        return false;
+      }
+    }
+  }
 
+  function isTurtleParty() {
+    let fiesta = 0;
+    if (fiesta === 2) {
+      fiesta = 0;
+      return true;
+    } else {
+      if (m === 30 && (s === 0 || s === 1)) {
+        fiesta = 0;
+        return true;
+      } else {
+        fiesta++;
+        console.log("cc2");
+        return false;
+      }
+    }
+  }
 
+  function isTurtlePooped() {
+    let wantToPoop = 0;
+    if (wantToPoop === 2) {
+      fiesta = 0;
+      return true;
+    } else {
+      if (m === 27 && (s === 0 || s === 1)
+      ){
+        wantToPoop = 0;
+        return true;
+      } else {
+        wantToPoop++;
+        console.log("cc3");
+        return false;
+      }
+    }
+  }
 
-// function isSleepyTurtle() {
-//   //let sleepy = 0
-//   if (hours === 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+  function isTurtleSleepy() {
+    //let sleepy = 0
+    if (h === 0 && m === 0 && s === 0) {
+      return true;
+    } else {
+      console.log("cc4");
+      return false;
+    }
+  }
 
-// function partyTurtle() {
-//   let fiesta = 0
-//   if (fiesta === 2) {
-//     fiesta = 0
-//     return true
-//   }
-//   else {
-//     if (minutes === 30){
-//       fiesta = 0
-//       return true
-//     }
-//     else {
-//       fiesta++
-//       console.log(fiesta)
-//       return false
-//     }
-//   }
-// };
+  const updateTurtle = () => {
+    if(isTurtlePooped()){
+      turtleImg.src = chrome.runtime.getURL("/imgTurtle/poopingTurtle.png")
+    };
+    if (isTurtleParty()){
+      turtleImg.src = chrome.runtime.getURL("/imgTurtle/partyTurtle.png")
+    };
+    if (isTurtleHungry()){
+      turtleImg.src = chrome.runtime.getURL("/imgTurtle/eatingTurtle.png")
+    };
+    if (isTurtleSleepy()){
+      turtleImg.src = chrome.runtime.getURL("/imgTurtle/sleepingTurtle.png")
+    }; 
+  };
 
-// function isTurtlePooped() {
-//   let wantToPoop = 0
-//   if ( wantToPoop === 2) {
-//     fiesta = 0
-//     return true
-//   }
-//   else {
-//     if (minutes === 15){
-//       wantToPoop = 0
-//       return true
-//     }
-//     else {
-//       wantToPoop++
-//       return false
-//     }
-//   }
-// };
+  startTime();
+  console.log(s)
+};
+
