128b2f96f425ba8d7a438d9f3799d068eda974b3
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2022-01-19 16:39:37 +0100

    Feat : playWith

--- src/turtle_pet/src/popup.js
+++ src/turtle_pet/src/popup.js
@@ -1,4 +1,15 @@
 let displayTurtle = document.getElementById("display");
+let displayplayWith = document.getElementById("playWith");
+
+
+//Execute script when clicked on popup's extension button "Joue avec moi"
+displayplayWith.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setplayWith,
+  });
+});
 
 //Execute script when clicked on popup's extension button "coucou"
 displayTurtle.addEventListener("click", async () => {
@@ -9,6 +20,12 @@ displayTurtle.addEventListener("click", async () => {
   });
 });
 
+
+
+
+
+
+
 // Display turtle on the html with div
 const setDisplayTurtle = () => {
   alert("cc");
@@ -16,21 +33,22 @@ const setDisplayTurtle = () => {
   turtleDiv.id = "turtlePet";
   turtleDiv.style = "z-index: 99999; position: fixed; top: 802px; right: 42px;";
   let turtleImg = document.createElement("img");
+  turtleImg.id = "turtleImg";
   turtleImg.src = chrome.runtime.getURL("/imgTurtle/idleTurtle.png");
   turtleDiv.appendChild(turtleImg);
   document.body.appendChild(turtleDiv);
-  
-  let today
-  let h
-  let m
-  let s 
 
-function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
+  let today;
+  let h;
+  let m;
+  let s;
+
+  function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
   }
-  return i;
-}
 
   function startTime() {
     // add a zero in front of numbers<10
@@ -40,16 +58,13 @@ function checkTime(i) {
     s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    t = setTimeout(function() {
-      startTime()
-    }, 2000);
+    t = setTimeout(function () {
+      startTime();
+    }, 1000);
 
     updateTurtle();
-  
   }
 
-
-  
   let randomNumber = Math.floor(Math.random() * 6);
 
   //All turtle's behaviors functions
@@ -60,14 +75,13 @@ function checkTime(i) {
       getHungry = 0;
       return true;
     } else {
-      if (m === 4  && s === 0) {
+      if (m === 37 && s === 0) {
         getHungry = 0;
         return true;
       } else {
         if ((m = 0 && s === 0)) {
           getHungry++;
         }
-        console.log("cc1");
         return false;
       }
     }
@@ -79,12 +93,11 @@ function checkTime(i) {
       fiesta = 0;
       return true;
     } else {
-      if (m === 30 && (s === 0 || s === 1)) {
+      if (m === 37 && s === 0) {
         fiesta = 0;
         return true;
       } else {
         fiesta++;
-        console.log("cc2");
         return false;
       }
     }
@@ -96,13 +109,11 @@ function checkTime(i) {
       fiesta = 0;
       return true;
     } else {
-      if (m === 27 && (s === 0 || s === 1)
-      ){
+      if (m === 27 && (s === 0 || s === 1)) {
         wantToPoop = 0;
         return true;
       } else {
         wantToPoop++;
-        console.log("cc3");
         return false;
       }
     }
@@ -113,27 +124,52 @@ function checkTime(i) {
     if (h === 0 && m === 0 && s === 0) {
       return true;
     } else {
-      console.log("cc4");
       return false;
     }
   }
 
   const updateTurtle = () => {
-    if(isTurtlePooped()){
-      turtleImg.src = chrome.runtime.getURL("/imgTurtle/poopingTurtle.png")
-    };
-    if (isTurtleParty()){
-      turtleImg.src = chrome.runtime.getURL("/imgTurtle/partyTurtle.png")
-    };
-    if (isTurtleHungry()){
-      turtleImg.src = chrome.runtime.getURL("/imgTurtle/eatingTurtle.png")
-    };
-    if (isTurtleSleepy()){
-      turtleImg.src = chrome.runtime.getURL("/imgTurtle/sleepingTurtle.png")
-    }; 
+    if (isTurtlePooped()) {
+      turtleImg.src = chrome.runtime.getURL("/imgTurtle/poopingTurtle.png");
+    }
+    if (isTurtleParty()) {
+      turtleImg.src = chrome.runtime.getURL("/imgTurtle/partyTurtle.png");
+    }
+    if (isTurtleHungry()) {
+      turtleImg.src = chrome.runtime.getURL("/imgTurtle/eatingTurtle.png");
+    }
+    if (isTurtleSleepy()) {
+      turtleImg.src = chrome.runtime.getURL("/imgTurtle/sleepingTurtle.png");
+    }
   };
 
   startTime();
-  console.log(s)
+  console.log(s);
 };
 
+// Display playWith turtle game
+function setplayWith() {
+  let turtleImg = document.getElementById("turtleImg");
+  let randomNumberOne = Math.floor(Math.random() * 99)
+  let randomNumberTwo = Math.floor(Math.random() * 99)
+  let result = parseInt(prompt (randomNumberOne + "+" + randomNumberTwo + "="));
+    if (result === (randomNumberOne + randomNumberTwo)){ 
+      turtleImg.src = chrome.runtime.getURL("/imgTurtle/idleTurtle.png"); 
+      console.log("good")
+    } 
+    else {
+      result = prompt ("try again !" + randomNumberOne + "+" + randomNumberTwo + "=");
+    }
+};
+
+
+
+
+/*let displayplayWith = document.getElementById("playWith");
+  alert("click sur moi la");
+  let clicks = 0; // counter
+  displayplayWith.addEventListener("click", function(e) {
+    e.preventDefault()
+    clicks++
+  });
+setTimeout(alert("Tu as cliqué" + clicks + "X !"), 20000)*/
